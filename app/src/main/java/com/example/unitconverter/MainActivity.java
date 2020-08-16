0260e7b0b209c4a69327ab8e79a845a52fabffa2
Neena Ugail <nhu21@bath.ac.uk>
2020-08-16 12:24:52 +0100

    added functionality to child spinners

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner categorySpinner, optionSpinner1, optionSpinner2;
+    ArrayAdapter<CharSequence> optionAdapter1, optionAdapter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +30,37 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        if (position == 0) {
 
+        if (position == 0) {
+            optionAdapter1 = ArrayAdapter.createFromResource(this, R.array.length, android.R.layout.simple_spinner_item);;
+            optionAdapter2 = ArrayAdapter.createFromResource(this, R.array.length, android.R.layout.simple_spinner_item);;
         }
 
         else if (position == 1) {
-            
+            optionAdapter1 = ArrayAdapter.createFromResource(this, R.array.mass, android.R.layout.simple_spinner_item);;
+            optionAdapter2 = ArrayAdapter.createFromResource(this, R.array.mass, android.R.layout.simple_spinner_item);;
         }
 
         else if (position == 2) {
-
+            optionAdapter1 = ArrayAdapter.createFromResource(this, R.array.volume, android.R.layout.simple_spinner_item);;
+            optionAdapter2 = ArrayAdapter.createFromResource(this, R.array.volume, android.R.layout.simple_spinner_item);;
         }
 
         else if (position == 3) {
-
+            optionAdapter1 = ArrayAdapter.createFromResource(this, R.array.time, android.R.layout.simple_spinner_item);;
+            optionAdapter2 = ArrayAdapter.createFromResource(this, R.array.time, android.R.layout.simple_spinner_item);;
         }
 
         else if (position == 4) {
-
+            optionAdapter1 = ArrayAdapter.createFromResource(this, R.array.temperature, android.R.layout.simple_spinner_item);;
+            optionAdapter2 = ArrayAdapter.createFromResource(this, R.array.temperature, android.R.layout.simple_spinner_item);;
         }
+
+        optionSpinner1.setAdapter(optionAdapter1);
+        optionSpinner2.setAdapter(optionAdapter2);
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
     }
 }
