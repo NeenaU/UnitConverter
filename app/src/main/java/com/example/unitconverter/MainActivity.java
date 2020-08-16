27f62d6866ce5f9b569d74ca6f62a75ef6786241
Neena Ugail <nhu21@bath.ac.uk>
2020-08-16 11:30:00 +0100

    made gui simpler

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -9,21 +9,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    Spinner categorySpinner, optionSpinner1, optionSpinner2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner categorySpinner = findViewById(R.id.categorySpinner);
+        categorySpinner = (Spinner)findViewById(R.id.categorySpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categorySpinner.setAdapter(adapter);
         categorySpinner.setOnItemSelectedListener(this);
+
+        optionSpinner1 = (Spinner)findViewById(R.id.optionSpinner1);
+        optionSpinner2 = (Spinner)findViewById(R.id.optionSpinner2);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+        if (position == 0) {
+
+        }
+
+        else if (position == 1) {
+            
+        }
+
+        else if (position == 2) {
+
+        }
+
+        else if (position == 3) {
+
+        }
+
+        else if (position == 4) {
+
+        }
     }
 
     @Override
